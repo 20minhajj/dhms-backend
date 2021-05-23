04b04cfe8f618290d0ab8e8339dc4569afc94453
20minhajj <minhajjharith@gmail.com>
2021-05-23 09:57:58 +0300

    securing the token once login

---
+++ helpers/jwt.js
@@ -0,0 +1,13 @@
+const expressjwt = require("express-jwt");
+
+const authJwt = () => {
+  const secret = process.env.SECRETE;
+  return expressjwt({
+    secret,
+    algorithms: ["HS256"],
+  }).unless({
+    path: ["/api/driver/auth/signup", "/api/driver/auth/signin"],
+  });
+};
+
+module.exports = authJwt;
