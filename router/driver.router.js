eee8c1edad77dbff8bb86f2922e0c021a59cbf17
20minhajj <minhajjharith@gmail.com>
2021-05-24 08:18:12 +0300

    Setting renaming and destination forlder

--- router/driver.router.js
+++ router/driver.router.js
@@ -1,11 +1,26 @@
 const express = require("express");
+const multer = require("multer");
 
 const router = express.Router();
 
 const registrationCtrl = require("../controllers/driver.controller");
 const loginCtrl = require("../controllers/driver.controller");
+const profilePicCtrl = require("../controllers/driver.controller");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads");
+  },
+  filename: function (req, file, cb) {
+    const fileName = file.originalname.split(" ").join("-");
+    cb(null, fileName + "-" + Date.now());
+  },
+});
+
+const uploadProfile = multer({ storage: storage });
 
 router.post("/driver/auth/signup", registrationCtrl.registration);
-router.post("/driver/auth/signin", loginCtrl.signin)
+router.post("/driver/auth/signin", loginCtrl.signin);
+router.put("/driver/setProfile/:id", uploadProfile.single('image'), profilePicCtrl.setProfilePic);
 
 module.exports = router;
