f6ebd4d95af3568e2014a3633edba495e84582a6
20minhajj <minhajjharith@gmail.com>
2021-05-31 20:16:16 +0300

    adding car routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const cors = require("cors");
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/errorHandler");
+const Role = db.role;
 
 // MIDDLEWARES
 app.use(express.json());
@@ -14,11 +15,15 @@ app.use(errorHandler);
 
 const DriverRoutes = require("./router/driver.router");
 const UserRoutes = require("./router/user.router");
+const CarRoutes = require("./router/car.router");
 // ROUTES
 app.use("/api", DriverRoutes);
 app.use("/api", UserRoutes);
+app.use("/api", CarRoutes);
+
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync");
+  initial();
 });
 
 const server = app.listen(process.env.PORT, () => {
@@ -26,3 +31,25 @@ const server = app.listen(process.env.PORT, () => {
   const port = server.address().port;
   console.log("App listening at http://%s:%s", host, port);
 });
+
+function initial() {
+  Role.create({
+    roleID: 1,
+    name: "ADMIN",
+  });
+
+  Role.create({
+    roleID: 2,
+    name: "USER",
+  });
+
+  Role.create({
+    roleID: 3,
+    name: "DRIVER",
+  });
+
+  Role.create({
+    roleID: 4,
+    name: "COOPRATES",
+  });
+}
