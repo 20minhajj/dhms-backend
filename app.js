833ea5941ecd446f5f96c0e36fa4b53b2f606307
20minhajj <minhajjharith@gmail.com>
2021-06-27 22:21:23 +0300

    adding routers packages

--- app.js
+++ app.js
@@ -1,25 +1,53 @@
 const express = require("express");
+// const session = require("cookie-session");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const helmet = require("helmet");
+const hpp = require("hpp");
+const csurf = require("csurf");
+const limiter = require("express-rate-limit");
 const db = require("./config/connection.config");
 const app = express();
 const cors = require("cors");
 const authJwt = require("./helpers/jwt");
 const errorHandler = require("./helpers/errorHandler");
+require("./helpers/passport")(app);
 const Role = db.role;
 
+/* Set Security Configs */
+app.use(helmet());
+app.use(hpp());
+//
 // MIDDLEWARES
+
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"],
+  })
+);
 app.options("*", cors);
 app.use(authJwt());
 app.use(errorHandler);
 
+app.use(csurf());
+
+app.use(limiter);
+
 const DriverRoutes = require("./router/driver.router");
 const UserRoutes = require("./router/user.router");
 const CarRoutes = require("./router/car.router");
+const OfferRoutes = require("./router/offers.router");
+const ReservationRoutes = require("./router/reservation.router");
 // ROUTES
 app.use("/api", DriverRoutes);
 app.use("/api", UserRoutes);
 app.use("/api", CarRoutes);
+app.use("/api", OfferRoutes);
+app.use("/api", ReservationRoutes);
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and Resync");
